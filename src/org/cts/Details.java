c8c66c7ced95f1a698bfea4600e6b7287da65a10
VishnuRadhakrishnann <vishnu31595@gmail.com>
2020-07-20 08:47:19 +0530

    commit message

---
+++ src/org/cts/Details.java
@@ -0,0 +1,28 @@
+package org.cts;
+
+public class Details {
+	
+	public static void main(String[] args) {
+		
+		byte a=25;
+		System.out.println(" My age is"+a);
+		short b=1234;
+		System.out.println("Student roll no is "+b);
+		int c=1234567890;
+		System.out.println("Account no is "+c);
+		long d=123456789012345678l;
+		System.out.println("my dubai moblie no is"+d);
+		float e=568965.3254f;
+		System.out.println("bank saving acc is"+e);
+		double f=213254687.4687654;
+		System.out.println("Total interest is"+f);
+		boolean g=true;
+		System.out.println("Student live in chennai"+g);
+		char h='@';
+		System.out.println("vishnu"+h+"gmail.com");
+		
+		String i="green@gmail.com";
+		System.out.println(i);
+	}
+
+}
